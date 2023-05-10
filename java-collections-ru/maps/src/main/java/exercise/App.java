b58d6aa4521cf650ae1bca8a74e6b5c154a19a04
@hexlet/cli <support@hexlet.io>
2023-05-10 21:57:39 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -2,17 +2,23 @@ package exercise;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
 
 // BEGIN
 class App {
     public static Map<String, Integer> getWordCount(String sentence) {
         Map<String, Integer> wordsCount = new HashMap<>();
-        String[] wordsInSentence = sentence.split(" ");
+        ArrayList<String> words = new ArrayList<>();
+        for(String w : sentence.split(" ")) {
+            words.add(w);
+        }
         int count = 1;
-        for (int i = 0; i < wordsInSentence.length - 1; i++) {
-            for (int j = i + 1; j < wordsInSentence.length; j++) {
-                if (wordsInSentence[i] == wordsInSentence[j]) {
+
+        for (int i = 0; i < words.size - 1; i++) {
+            for (int j = i + 1; j < words.size; j++) {
+                if (wordsInSentence[i].equals(wordsInSentence[j])) {
                     count += 1;
+                    words.remove(j);
                 }
             }
             wordsCount.put(wordsInSentence[i], count);
