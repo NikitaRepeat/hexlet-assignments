f30fadcb4e60d2388ac93b8b0ac1b4ffaf588a30
@hexlet/cli <support@hexlet.io>
2023-05-18 15:28:41 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -21,21 +21,18 @@ public class App {
                 .filter(s -> s.startsWith("X_FORWARDED_"))
                 .collect(Collectors.toList());
 
+
         // Конвертируем переменные в строку формата "имя1=значение1,имя2=значение2,имя3=значение3,..."
         for (String var : variables) {
             String[] parts = var.split("=");
             if (parts.length == 2) {
                 String name = parts[0].replace("X_FORWARDED_", "");
                 String value = parts[1].trim();
-                result += name + "=" + value;
-            } else {
-                String name = parts[0].replace("X_FORWARDED_", "");
-                String value = parts[1].trim();
                 result += name + "=" + value + ",";
             }
         }
         // Удаляем последнюю запятую и возвращаем результат
-        return result.substring(0, result.length() - 1);
+        return result;
     }
 }
 //END
