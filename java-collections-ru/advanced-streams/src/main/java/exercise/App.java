d30671c20641dc4ca6a2474329d266c211a67bb5
@hexlet/cli <support@hexlet.io>
2023-05-18 15:04:28 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -2,20 +2,36 @@ package exercise;
 
 import java.util.stream.Collectors;
 import java.util.Arrays;
+import java.util.List;
 
 // BEGIN
 public class App {
     public static String getForwardedVariables(String content) {
-        return Arrays.stream(content.split("\n"))
-                .filter(line -> line.contains("environment"))
-                .flatMap(line -> Arrays.stream(line.split("\"")))
-                .filter(variable -> variable.startsWith("X_FORWARDED_"))
-                .map(variable -> variable.replace("X_FORWARDED_", ""))
-                .map(variable -> {
-                    String[] variableValue = variable.split("=");
-                    return variableValue[0] + "=" + variableValue[1];
-                })
-                .collect(Collectors.joining(","));
+        String result = "";
+
+        // Ищем строки с командой environment
+        List<String> envStrings = Arrays.asList(content.split("\n"))
+                .stream()
+                .filter(s -> s.startsWith("environment="))
+                .collect(Collectors.toList());
+
+        // Извлекаем переменные
+        List<String> variables = envStrings.stream()
+                .flatMap(s -> Arrays.asList(s.split(",")).stream())
+                .filter(s -> s.startsWith("X_FORWARDED_"))
+                .collect(Collectors.toList());
+
+        // Конвертируем переменные в строку формата "имя1=значение1,имя2=значение2,имя3=значение3,..."
+        for (String var : variables) {
+            String[] parts = var.split("=");
+            if (parts.length == 2) {
+                String name = parts[0].replace("X_FORWARDED_", "");
+                String value = parts[1].trim();
+                result += name + "=" + value + ",";
+            }
+        }
+        // Удаляем последнюю запятую и возвращаем результат
+        return result.substring(0, result.length() - 1);
     }
 }
 //END
