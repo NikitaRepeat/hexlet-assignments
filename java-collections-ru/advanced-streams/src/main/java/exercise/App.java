4d93c9b44b1948f3304630f23f54e20ffdf3508d
@hexlet/cli <support@hexlet.io>
2023-05-18 16:20:45 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -7,7 +7,6 @@ import java.util.List;
 // BEGIN
 public class App {
     public static String getForwardedVariables(String content) {
-        String result = "";
 
         // Ищем строки с командой environment
         List<String> envStrings = Arrays.asList(content.split("\n"))
@@ -21,6 +20,8 @@ public class App {
                 .filter(s -> s.startsWith("X_FORWARDED_"))
                 .collect(Collectors.toList());
 
+        StringBuilder sb = new StringBuilder();
+
 
         // Конвертируем переменные в строку формата "имя1=значение1,имя2=значение2,имя3=значение3,..."
         for (String var : variables) {
@@ -28,11 +29,11 @@ public class App {
             if (parts.length == 2) {
                 String name = parts[0].replace("X_FORWARDED_", "");
                 String value = parts[1].trim();
-                result += name + "=" + value + ",";
+                sb.append(name).append("=").append(value);
             }
         }
         // Удаляем последнюю запятую и возвращаем результат
-        return result;
+        return sb.toString();
     }
 }
 //END
